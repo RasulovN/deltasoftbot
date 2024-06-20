c4c89927cee2229d5c5fcaf94b14ebebbd80485c
Nurbek <92941063+RasulovN@users.noreply.github.com>
2024-06-20 09:17:44 +0500

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const mongoURI = process.env.MONGODB_URI;
 mongoose
   .connect(mongoURI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
+    // useUnifiedTopology: true,
   })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log("Error connecting to MongoDB:", err));
